914be9c08b1d8f1c96b9182d26b87e718289812c
FDCTech <chbk-mac@forexdevelopment.com>
2021-12-04 01:01:46 +0500

    Add Nez template with Screen

--- Tetramonio/Program.cs
+++ Tetramonio/Program.cs
@@ -7,7 +7,7 @@ namespace Tetramonio
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameRoot())
                 game.Run();
         }
     }
---
+++ Tetramonio/Scenes/GameplayScene.cs
@@ -0,0 +1,35 @@
+using Nez;
+using Nez.ImGuiTools;
+
+namespace Tetramonio.Scenes
+{
+    public class GameplayScene : Scene
+    {
+        public const int ScreenSpaceRenderLayer = 999;
+
+        public readonly int Width = 1280;
+        public readonly int Height = 720;
+
+        public GameplayScene()
+        {
+            AddRenderer(new ScreenSpaceRenderer(100, ScreenSpaceRenderLayer));
+            AddRenderer(new RenderLayerExcludeRenderer(0, ScreenSpaceRenderLayer));
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            var imGuiManager = new ImGuiManager();
+            Core.RegisterGlobalManager(imGuiManager);
+            
+            SetDesignResolution(Width, Height, SceneResolutionPolicy.ShowAllPixelPerfect);
+            Screen.SetSize(Width, Height);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
