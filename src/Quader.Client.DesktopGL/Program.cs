ad5c417234c6be5e95f025100e96467001541353
loonacuse <loonacuse@gmail.com>
2022-07-31 22:55:27 +0500

    [Client] Add Assembly & Game version

--- src/Quader.Client.DesktopGL/Program.cs
+++ src/Quader.Client.DesktopGL/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Reflection;
 using Quader.Debugging.Logging;
 using Quader.Debugging.Logging.Loggers;
 using Thread = System.Threading.Thread;
@@ -24,9 +25,18 @@ namespace Quader
 
             var logger = LoggerFactory.GetLogger("Kernel");
 
-            logger.Info("\nStarting up Quader\n" +
-                        $"Startup Time: {DateTimeOffset.Now}\n" +
-                        $"Working Directory: {WorkingDirectory}\n");
+            var asm = Assembly.GetEntryAssembly();
+            var version = asm?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "UNKNOWN";
+            var asmVer = asm?.GetName()?.Version?.ToString() ?? "UNKNOWN";
+
+            logger.Info($"\n\n" +
+                        $" =======================================================\n" +
+                        $" = Starting up Quader\n" +
+                        $" = Startup Time: {DateTimeOffset.Now}\n" +
+                        $" = Working Directory: {WorkingDirectory}\n" +
+                        $" = Assembly Version: {asmVer}\n" +
+                        $" = Game Version: {version}\n" +
+                        $" =======================================================\n");
 
             AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
             {
@@ -42,6 +52,8 @@ namespace Quader
             
             using var game = new GameRoot();
             game.Run();
+
+            logger.Info("Game Closed");
         }
     }
 }
