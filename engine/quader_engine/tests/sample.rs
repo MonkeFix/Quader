7d0f0104a51d2f84b1b1d7a601398921c4fac2cd
Grigory Alfyorov <loonacuse@gmail.com>
2023-09-13 03:26:16 +0500

    Add boilerplate Rust projects (engine and engine samples)

---
+++ engine/quader_engine/tests/sample.rs
@@ -0,0 +1,10 @@
+﻿#[cfg(test)]
+mod tests {
+    use quader_engine::add;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ engine/quader_engine_samples/src/main.rs
@@ -0,0 +1,8 @@
+use quader_engine;
+
+fn main() {
+    println!("Hello, world!");
+    
+    let res = quader_engine::add(5, 6);
+    println!("5 + 6 = {res}");
+}
