72405cfbc9be601e559057e47f284c11f2c3a385
Grigory Alfyorov <loonacuse@gmail.com>
2023-09-24 02:10:19 +0500

    [Core] Add Dear ImGUI crate to samples

--- core/quader_engine_samples/src/main.rs
+++ core/quader_engine_samples/src/main.rs
@@ -1,46 +1,108 @@
 extern crate sdl2;
 
-use std::time::Duration;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use std::time::Instant;
+use glow::HasContext;
+use imgui::Context;
+use imgui_glow_renderer::AutoRenderer;
+use imgui_sdl2::ImguiSdl2;
+use sdl2::{
+    event::Event,
+    video::{GLProfile, Window},
+};
 
-fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
+// Create a new glow context.
+fn glow_context(window: &Window) -> glow::Context {
+    unsafe {
+        glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
+    }
+}
 
-    let windows = video_subsystem
-        .window("Quader Engine Samples", 800, 600)
+fn main() {
+    /* initialize SDL and its video subsystem */
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+
+    /* hint SDL to initialize an OpenGL 3.3 core profile context */
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_version(3, 3);
+    gl_attr.set_context_profile(GLProfile::Core);
+
+    /* create a new window, be sure to call opengl method on the builder when using glow! */
+    let window = video_subsystem
+        .window("Hello imgui-rs!", 1280, 720)
+        .allow_highdpi()
+        .opengl()
         .position_centered()
-        .vulkan()
+        .resizable()
         .build()
-        .map_err(|e| e.to_string())?;
+        .unwrap();
 
-    let mut canvas = windows.into_canvas().build().map_err(|e| e.to_string())?;
+    /* create a new OpenGL context and make it current */
+    let gl_context = window.gl_create_context().unwrap();
+    window.gl_make_current(&gl_context).unwrap();
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
+    /* enable vsync to cap framerate */
+    window.subsystem().gl_set_swap_interval(1).unwrap();
 
-    let mut event_pump = sdl_context.event_pump()?;
+    /* create new glow and imgui contexts */
+    let gl = glow_context(&window);
 
-    'running: loop {
+    /* create context */
+    let mut imgui = Context::create();
+
+    /* disable creation of files on disc */
+    imgui.set_ini_filename(None);
+    imgui.set_log_filename(None);
+
+    /* setup platform and renderer, and fonts to imgui */
+    imgui
+        .fonts()
+        .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
+
+    /* create platform and renderer */
+    let mut platform = ImguiSdl2::new(&mut imgui, &window);
+    let mut renderer = AutoRenderer::initialize(gl, &mut imgui).unwrap();
+
+    /* start main loop */
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    let mut last_frame = Instant::now();
+
+    'main: loop {
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
+            /* pass all events to imgui platfrom */
+            platform.handle_event(&mut imgui, &event);
+            if platform.ignore_event(&event) { continue; }
+
+            if let Event::Quit { .. } = event {
+                break 'main;
             }
         }
 
-        canvas.clear();
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
-    }
+        /* call prepare_frame before calling imgui.new_frame() */
+        platform.prepare_frame(imgui.io_mut(), &window, &event_pump.mouse_state());
 
-    Ok(())
+        let now = Instant::now();
+        let delta = now - last_frame;
+        let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
+        last_frame = now;
+        imgui.io_mut().delta_time = delta_s;
+
+        let ui = imgui.frame(); // new_frame();
+        /* create imgui UI here */
+        ui.show_demo_window(&mut true);
+
+        /* render */
+
+        unsafe { renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
+
+        platform.prepare_render(&ui, &window);
+        let draw_data = imgui.render();
+        renderer.render(draw_data).unwrap();
+
+        window.gl_swap_window();
+
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
