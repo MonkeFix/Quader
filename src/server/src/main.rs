8f47f34988702f9546d90f8a516b1f3897253217
Grigory Alfyorov <loonacuse@gmail.com>
2024-01-04 18:00:03 +0500

    Playing with server

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -10,8 +10,16 @@ async fn main() {
         .and_then(handler::ws);
 
     let index = warp::path::end().map(|| warp::reply::html(index_html));
+    let lobby = warp::path!("lobby" / String)
+        .map(|name| {
+            //format!("Hello, {}", name)
+            let arr = vec![1,2,3,4];
+            warp::reply::json(&arr)
+        }); 
 
-    let routes = index.or(chat);
+    
+
+    let routes = index.or(chat).or(lobby);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
