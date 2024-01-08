c81231f5db566e658767e2c9e54413d21b44f1c3
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-08 19:28:24 +0300

    [web-api] Create initial main file

--- src/web-api/src/main.rs
+++ src/web-api/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+
+ #[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    Ok(())
 }
