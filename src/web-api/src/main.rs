afcc34330e42d2077c3d73572ee8b4c9007bcfeb
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-08 19:28:24 +0300

    [web-api] Create initial main file

--- src/web-api/src/main.rs
+++ src/web-api/src/main.rs
@@ -1,8 +1,14 @@
 /*
- * Copyright (c) Grigory Alfyorov. Licensed under the MIT License.
+ * Copyright (c) MonkeFix. Licensed under the MIT License.
  * See the LICENCE file in the repository root for full licence text.
  */
 
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    Ok(())
 }
