ac8b50fc245d34c25bb3e4593ca00657a8a011fc
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-08 19:43:31 +0300

    [web-api] Create initial db-related module

---
+++ src/web-api/src/db.rs
@@ -0,0 +1,26 @@
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+
+use crate::config::Config;
+
+#[derive(Debug, Clone)]
+pub struct DBClient {
+    pool: Pool<Postgres>,
+}
+
+impl DBClient {
+    pub async fn init(config: &Config) -> Self {
+        let max_conn_var = "DB_POOL_MAX_CONNECTIONS";
+        let max_connections = std::env::var(max_conn_var)
+            .expect("{max_conn_var} must be set")
+            .parse::<u32>()
+            .expect("{max_conn_var} must be valid integer");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(max_connections)
+            .connect(&config.database_url)
+            .await
+            .expect("Failed to initialize database connection pool.");
+
+        DBClient { pool }
+    }
+}
