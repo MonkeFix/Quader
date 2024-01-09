9a309584afb32aa862880ada89698c3bd167c480
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-09 15:21:09 +0300

    [web-api] Introduce logout endpoint

--- src/web-api/src/scope.rs
+++ src/web-api/src/scope.rs
@@ -8,4 +8,8 @@ pub fn auth() -> Scope {
     web::scope("/auth")
         .service(self::auth::handler::register)
         .service(self::auth::handler::login)
+        .service(web::scope("")
+                    .service(self::auth::handler::logout)
+                    .wrap(RequireAuth)
+        )
 }
