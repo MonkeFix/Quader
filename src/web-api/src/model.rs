d0ec6b0641255334ce4d34d0e6149fd1e5b9f6fc
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-09 12:22:22 +0300

    [web-api] Create user model

---
+++ src/web-api/src/model.rs
@@ -0,0 +1,33 @@
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+use derive_more::Display;
+
+#[derive(Debug, Deserialize, Serialize, Clone, Copy, sqlx::Type, PartialEq, Display)]
+#[sqlx(type_name = "user_role", rename_all = "lowercase")]
+pub enum UserRole {
+    #[display(fmt = "admin")]
+    Admin,
+    #[display(fmt = "user")]
+    User,
+    #[display(fmt = "moderator")]
+    Moderator,
+    #[display(fmt = "supporter")]
+    Supporter,
+    #[display(fmt = "guest")]
+    Guest,
+}
+
+#[derive(Debug, Deserialize, sqlx::FromRow, sqlx::Type, Serialize, Clone)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub username: String,
+    pub email: String,
+    pub password_hash: String,
+    pub role: UserRole,
+    pub photo: String,
+    pub verified: bool,
+    #[serde(rename = "createdAt")]
+    pub created_at: DateTime<Utc>,
+    #[serde(rename = "updatedAt")]
+    pub updated_at: DateTime<Utc>,
+}
