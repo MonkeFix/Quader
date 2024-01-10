ba86dc6472b02e58d541fc9786c58fcbc20460ae
Grigory Alfyorov <loonacuse@gmail.com>
2024-01-11 00:23:22 +0500

    [server] Load config from .env

---
+++ src/server/src/config.rs
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Grigory Alfyorov. Licensed under the MIT License.
+ * See the LICENCE file in the repository root for full licence text.
+ */
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub web_api_url: String
+}
+
+impl Config {
+    pub fn init() -> Config {
+        let web_api_url = std::env::var("WEB_API_URL").expect("WEB_API_URL must be set");
+
+        Config {
+            web_api_url
+        }
+    }
+}
