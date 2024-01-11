01c8a3a752e96c92154330adab3c4d5a3149e8ab
Grigory Alfyorov <loonacuse@gmail.com>
2024-01-11 15:29:44 +0500

    Remove server-prototype

--- src/server-prototype/src/main.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Grigory Alfyorov. Licensed under the MIT License.
- * See the LICENCE file in the repository root for full licence text.
- */
-
-fn main() {
-    println!("Hello, world!");
-}
