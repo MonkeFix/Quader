b7ddae5e6453a91823d040f1b1482b52fd751d71
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-13 19:34:03 +0300

    [web-api] Add authentication for swagger

--- src/web-api/src/openapi.rs
+++ src/web-api/src/openapi.rs
@@ -1,6 +1,9 @@
-use utoipa::OpenApi;
+use utoipa::{
+    openapi::security::{HttpAuthScheme, HttpBuilder, SecurityScheme},
+    Modify, OpenApi,
+};
 
-use crate::{dto, error, http, model, scope};
+use crate::{dto, error, model, scope};
 
 #[derive(OpenApi)]
 #[openapi(
@@ -14,6 +17,24 @@ use crate::{dto, error, http, model, scope};
         error::Response,
         error::Status,
         error::Error,
-    ))
+    )),
+    modifiers(&SecurityAddon)
 )]
 pub struct ApiDoc;
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        let components = openapi.components.as_mut().unwrap();
+        components.add_security_scheme(
+            "token",
+            SecurityScheme::Http(
+                HttpBuilder::new()
+                    .scheme(HttpAuthScheme::Bearer)
+                    .bearer_format("JWT")
+                    .build(),
+            ),
+        )
+    }
+}
