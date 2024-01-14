5fddc20ea2c8a21ecbfad4662a5e8ed1903c29f7
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-14 20:59:28 +0300

    [web-api] Improve validation on login and register

--- src/web-api/src/dto.rs
+++ src/web-api/src/dto.rs
@@ -4,7 +4,10 @@ use validator::Validate;
 
 #[derive(Validate, Debug, Default, Clone, Deserialize, ToSchema)]
 pub struct RegisterUser {
-    #[validate(length(min = 1, message = "Name is required"))]
+    #[validate(
+        length(min = 1, message = "Username is required"),
+        length(max = 24, message = "Username is too long")
+    )]
     pub username: String,
     #[validate(
         length(min = 1, message = "Email is required"),
@@ -34,7 +37,8 @@ pub struct LoginUser {
     pub email: String,
     #[validate(
         length(min = 1, message = "Password is required"),
-        length(min = 6, message = "Password must be at least 6 characters")
+        length(min = 6, message = "Password must be at least 6 characters"),
+        length(max = 64, message = "Password must not exceed 64 characters")
     )]
     pub password: String,
 }
