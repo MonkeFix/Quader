b78d9e05fb7a0b399f619872c0400e3f2378d32b
Savely Krendelhoff <alicecandyom@proton.me>
2024-01-16 19:34:38 +0300

    [web-api] Add refresh token maxage env var

--- src/web-api/src/config.rs
+++ src/web-api/src/config.rs
@@ -3,6 +3,7 @@ pub struct Config {
     pub database_url: String,
     pub jwt_secret: String,
     pub jwt_maxage: i64,
+    pub jwt_refresh_maxage: i64,
     pub port: u16,
 }
 
@@ -18,11 +19,17 @@ impl Config {
             .expect("JWT_MAXAGE must be set")
             .parse::<u32>()
             .expect("JWT_MAXAGE must be valid integer") as i64;
+        let jwt_refresh_maxage = std::env::var("JWT_REFRESH_MAXAGE")
+            .expect("JWT_REFRESH_MAXAGE must be set")
+            .parse::<u32>()
+            .expect("JWT_REFRESH_MAXAGE must be valid integer")
+            as i64;
 
         Config {
             database_url,
             jwt_secret,
             jwt_maxage,
+            jwt_refresh_maxage,
             port,
         }
     }
