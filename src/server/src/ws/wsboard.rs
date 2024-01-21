91999187051897d67aa98b17561ac6826ebd5d6d
Grigory Alfyorov <loonacuse@gmail.com>
2024-01-21 09:23:25 +0500

    [server] Update WsBoardMgr's update method

--- src/server/src/ws/wsboard.rs
+++ src/server/src/ws/wsboard.rs
@@ -1,9 +1,13 @@
-use std::{collections::HashMap, sync::Arc, time::Instant};
+use std::{
+    collections::HashMap,
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use quader_engine::{board::Board, time_mgr::TimeMgr, wall_kick_data::WallKickData};
 use rand::{thread_rng, RngCore};
 use tokio::{
-    select,
+    pin, select,
     sync::{mpsc, oneshot},
 };
 
@@ -43,37 +47,31 @@ impl WsBoardMgr {
     pub async fn run(mut self) {
         let (terminate_tx, terminate_rx) = oneshot::channel();
         self.terminate_tx = Some(terminate_tx);
+        let mut interval = tokio::time::interval(Duration::from_millis(333));
 
         self.is_started = true;
 
-        let update = async {
-            let mut prev = Instant::now();
-            let mut lag = 0.0;
+        let mut prev = Instant::now();
 
-            loop {
-                let curr = Instant::now();
-                let elapsed = curr - prev;
-                prev = curr;
-                lag += elapsed.as_secs_f32();
+        loop {
+            let tick = interval.tick();
+            pin!(tick);
 
-                // TODO: process input
+            let now = Instant::now();
+            let dt = now - prev;
+            prev = now;
 
-                while lag >= MS_PER_UPDATE {
-                    self.time_mgr.update(lag);
-                    //TODO:
-                    self.update_boards();
-                    //log::info!("update, lag: {lag}, elapsed: {}", elapsed_sec);
-                    lag -= MS_PER_UPDATE;
+            select! {
+                /* _ = update => {
+
                 }
-            }
-        };
-
-        select! {
-            _ = update => {
-
-            }
-            _ = terminate_rx => {
-                log::info!("terminating board manager");
+                _ = terminate_rx => {
+                    log::info!("terminating board manager");
+                } */
+                _ = tick => {
+                    //log::debug!("TICK, dt: {:?}", dt.as_secs_f32());
+                    self.time_mgr.update(dt.as_secs_f32());
+                }
             }
         }
     }
